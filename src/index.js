b84809aebd1129e667f09113b2d4bd2751eecb89
Кирилл Поздняков <crm.c.29974@dz.com>
2024-04-02 09:06:15 +0300

    commentedd functions

--- src/index.js
+++ src/index.js
@@ -10,50 +10,50 @@ import { MailtrapClient } from "mailtrap";
 // const SENDER_EMAIL = "<SENDER@YOURDOMAIN.COM>";
 // const RECIPIENT_EMAIL = "<RECIPIENT@EMAIL.COM>";
 
-async function sendWithMailTrap() {
-  const client = new MailtrapClient({ token: 'b342ac29b797a460e09faf770f1d3afd' });
+// async function sendWithMailTrap() {
+//   const client = new MailtrapClient({ token: 'b342ac29b797a460e09faf770f1d3afd' });
 
-  const sender = { email: 'sender.email.supp@gmail.com' };
+//   const sender = { email: 'sender.email.supp@gmail.com' };
 
-  try {
-    await client
-      .send({
-        from: sender,
-        to: [{ email: 'pozdniakovkyryl@gmail.com' }],
-        subject: "Hello from Mailtrap!",
-        text: "Welcome to Mailtrap Sending!",
-      })
-  } catch (error) {
-    console.log(error);
-  }
-}
+//   try {
+//     await client
+//       .send({
+//         from: sender,
+//         to: [{ email: 'pozdniakovkyryl@gmail.com' }],
+//         subject: "Hello from Mailtrap!",
+//         text: "Welcome to Mailtrap Sending!",
+//       })
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 
-async function sendWithNodemailer() { // send with nodemailer and mailtrap
-  const transporter = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "49295814a1a65c",
-      pass: "b342ac29b797a460e09faf770f1d3afd"
-    }
-  })
+// async function sendWithNodemailer() { // send with nodemailer and mailtrap
+//   const transporter = nodemailer.createTransport({
+//     host: "sandbox.smtp.mailtrap.io",
+//     port: 2525,
+//     auth: {
+//       user: "49295814a1a65c",
+//       pass: "b342ac29b797a460e09faf770f1d3afd"
+//     }
+//   })
 
-  try {
-    const info = await transporter.sendMail({
-      from: 'sender.email.supp@gmail.com', // sender address
-      to: "pozdniakovkyryl@gmail.com'", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
-    });
+//   try {
+//     const info = await transporter.sendMail({
+//       from: 'sender.email.supp@gmail.com', // sender address
+//       to: "pozdniakovkyryl@gmail.com'", // list of receivers
+//       subject: "Hello ✔", // Subject line
+//       text: "Hello world?", // plain text body
+//       html: "<b>Hello world?</b>", // html body
+//     });
 
-    console.log(info.messageId);
+//     console.log(info.messageId);
 
-  } catch (error) {
-    console.log('error sendWithNodemailer', error)
-  }
-}
+//   } catch (error) {
+//     console.log('error sendWithNodemailer', error)
+//   }
+// }
 
 async function sendWith2FANodemailer() {
   const transporter = nodemailer.createTransport({
